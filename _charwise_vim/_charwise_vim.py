17050607114405c66a89811de57633bcc2ef60af
Dylan Chong <dylan.chong@icloud.com>
2017-07-04 22:15:28 +1200

    Tidy up, add SimpleCommandRule with a few commands

--- _charwise_vim/_charwise_vim.py
+++ _charwise_vim/_charwise_vim.py
@@ -3,7 +3,9 @@ import aenea.misc
 import aenea.vocabulary
 
 from aenea import (
-    Key
+    Key,
+    NoAction,
+    Text
 )
 
 from aenea.proxy_contexts import ProxyAppContext
@@ -12,12 +14,12 @@ from dragonfly import (
     Alternative,
     AppContext,
     CompoundRule,
+    Dictation,
     Grammar,
     MappingRule,
     Repetition,
     RuleRef
 )
-
 
 # TODO remove this?
 GRAMMAR_NAME = 'charwise_vim'
@@ -27,34 +29,83 @@ CHAR_KEY_MAPPINGS = {  # TODO move this into an aenea file?
     # In each sub-key: What to say (key): Dragon key code (value)
     'all': {},  # Updated below
     'uppercase_letters': None,  # Updated below
+    # TODO control/alt
     'lowercase_letters': {
-        'alpha': 'a', 'bravo': 'b', 'charlie': 'c', 'delta': 'd',
-        'echo': 'e', 'foxtrot': 'f', 'golf': 'g', 'hotel': 'h',
-        'indigo': 'i', 'juliet': 'j', 'kilo': 'k', 'lima': 'l',
-        'mike': 'm', 'november': 'n', 'oscar': 'o', 'poppa': 'p',
-        'quiche': 'q', 'romeo': 'r', 'sierra': 's', 'tango': 't',
-        'uniform': 'u', 'victor': 'v', 'whiskey': 'w', 'x-ray': 'x',
-        'yankee': 'y', 'zulu': 'z'
+        'alpha': 'a',
+        'bravo': 'b',
+        'charlie': 'c',
+        'delta': 'd',
+        'echo': 'e',
+        'foxtrot': 'f',
+        'golf': 'g',
+        'hotel': 'h',
+        'indigo': 'i',
+        'juliet': 'j',
+        'kilo': 'k',
+        'lima': 'l',
+        'mike': 'm',
+        'november': 'n',
+        'oscar': 'o',
+        'poppa': 'p',
+        'quiche': 'q',
+        'romeo': 'r',
+        'sierra': 's',
+        'tango': 't',
+        'uniform': 'u',
+        'victor': 'v',
+        'whiskey': 'w',
+        'x-ray': 'x',
+        'yankee': 'y',
+        'zulu': 'z'
         },
     'symbols': {
-        'left paren': 'lparen',
-        'right paren': 'rparen',
+        # Brackets and stuff
+        'left (paren|parenthesis)': 'lparen',
+        'right (paren|parenthesis)': 'rparen',
         'left bracket': 'lbracket',
         'right bracket': 'rbracket',
         'left brace': 'lbrace',
         'right brace': 'rbrace',
-        'quote': 'squote',
+        '(less than|left angle)': 'leftangle',
+        '(greater than|right angle)': 'rightangle',
+        # Quotes
+        '[single] quote': 'squote',
         'double quote': 'dquote',
+        'backtick': 'backtick',
+        # Slashes
         'backslash': 'backslash',
         'forward slash': 'slash',
-        'full stop': 'dot',
+        # Shift + Keys 1-8
+        'exclamation [mark]': 'exclamation',
+        'at [sign]': 'at',
+        # TODO LATER finish keys 3-8
+        # Spaces
+        'space [bar]': 'space',
+        'tab': 'tab',
+        # Other
+        '(full stop|dot)': 'dot',
         'comma': 'comma',
+        'question [mark]': 'question',
+        '(dash|hyphen|minus)': 'minus',
+        '(underscore|score)': 'minus',
+        'equals': 'equals',
+        'plus': 'plus',
+        'colon': 'colon',
+        'semicolon': 'semicolon',
+        '[vertical] bar': 'bar',
         },
     'digits': {
         ('dig ' + word): num for word, num in {
-            'zero': '0', 'one': '1', 'two': '2', 'three': '3',
-            'four': '4', 'five': '5', 'six': '6', 'seven': '7',
-            'eight': '8', 'niner': '9'
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'niner': '9'
             }.iteritems()
         }
     }
@@ -74,11 +125,7 @@ def load():
 
 
 def unload():
-    aenea.vocabulary.uninhibit_global_dynamic_vocabulary(
-        GRAMMAR_NAME, GRAMMAR_TAGS
-        )
-    for tag in GRAMMAR_TAGS:
-        aenea.vocabulary.unregister_dynamic_vocabulary(tag)
+    aenea.vocabulary.inhibit_global_dynamic_vocabulary(GRAMMAR_NAME, GRAMMAR_TAGS)
 
     global charwise_grammar
     if charwise_grammar:
@@ -90,10 +137,6 @@ def setup_key_mappings():
     # See the Dragonfly documentation to see what the values should be:
     # http://dragonfly.readthedocs.io/en/latest/_modules/dragonfly/actions/action_key.html?highlight=lparen
     # Not all symbols are here. Feel free to add the ones you need
-
-    # TODO NEXT USE and test (lower, upper ,symbol, digit)
-    # TODO AFTER test vim grammar real time
-
     CHAR_KEY_MAPPINGS['uppercase_letters'] = {
         UPPERCASE_KEY_PREFIX + speech: 's-' + letter
         for speech, letter in CHAR_KEY_MAPPINGS['lowercase_letters'].iteritems()
@@ -106,13 +149,7 @@ def setup_key_mappings():
 def setup_grammar():
     vim_context = create_app_context()
     new_grammar = Grammar(GRAMMAR_NAME, context=vim_context)
-
-    # Only use the grammar for vim_context # TODO remove?
-    aenea.vocabulary.inhibit_global_dynamic_vocabulary(
-        GRAMMAR_NAME, GRAMMAR_TAGS, context=vim_context
-        )
-    # top_level_rule =
-    # aenea.vocabulary.register_dynamic_vocabulary(GRAMMAR_TAGS[0]) #TODO
+    aenea.vocabulary.uninhibit_global_dynamic_vocabulary(GRAMMAR_NAME, GRAMMAR_TAGS)
 
     new_grammar.add_rule(CharwiseVimRule())
     new_grammar.load()
@@ -147,9 +184,28 @@ class SingleCharRule(MappingRule):
     """
     setup_key_mappings()
     mapping = CHAR_KEY_MAPPINGS['all']
+    mapping['down'] = 'j'
 
     def value(self, node):
         return Key(MappingRule.value(self, node))
+
+
+class SimpleCommandRule(MappingRule):
+    """
+    Similar to SingleCharRule, but you can include non-symbol things like
+    pressing 'Escape', or 'Control-D'.
+    """
+    setup_key_mappings()
+    mapping = {
+        '(escape|quit)': Key('escape'),
+        'backspace': Key('backspace'),
+        'up': Key('up'),
+        'down': Key('down'),
+        'left': Key('left'),
+        'right': Key('right'),
+        '(page up|gup)': Key('c-u'),
+        '(page down|gone)': Key('c-d'),
+    }
 
 
 class CharwiseVimRule(CompoundRule):
@@ -159,17 +215,17 @@ class CharwiseVimRule(CompoundRule):
     Allows for saying multiple commands at once, but will end
     after dictating words (see :class:`~TextEntryRule`)
     """
-    repeated_rules_key = 'multiple_repeatable_rules'
-    spec = '[<{}>]'.format(repeated_rules_key)  # TODO End with TextEntryRule
+    repeated_rules_key = 'repeated_rules'
+    spec = '<{}>'.format(repeated_rules_key)  # TODO End with TextEntryRule
     _repeatable_rules = [
-        # TODO replace with count
-        RuleRef(SingleCharRule())
-        # TODO add commands
+        RuleRef(SingleCharRule()),
+        RuleRef(SimpleCommandRule()),
         ]
     extras = [
+        # TODO put with count
         Repetition(
             Alternative(_repeatable_rules),
-            max=2,
+            max=20,
             name=repeated_rules_key
             )
         ]
@@ -182,5 +238,5 @@ class CharwiseVimRule(CompoundRule):
             print 'Executing {}'.format(key)
             key.execute()
 
-
 load()
+
